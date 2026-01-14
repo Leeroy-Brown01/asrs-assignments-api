5d9216820e102a7a1b6e8ecc62d510a15728ee63
Leeroy Brown <brownleeroy010@gmail.com>
2026-01-14 18:59:03 +0200

    initial commit

---
+++ src/main/java/com/brown/asrs_assignments_api/AsrsAssignmentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_assignments_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrsAssignmentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrsAssignmentsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brown/asrs_assignments_api/AsrsAssignmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_assignments_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrsAssignmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
